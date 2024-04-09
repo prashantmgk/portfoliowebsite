8c224a497a6dea88333ba861a300bad198892db8
prashantmgk <prashantmgk@gmail.com>
2024-04-09 13:46:54 +0545

    changed CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import LinkedinIcon from '../assets/icons/linkedin-icon.png'
 import XIcon from '../assets/icons/x-icon.png'
 import DirectDonwloadIcon from '../assets/icons/direct-download.png'
 import HeroImage from '../assets/images/hero-image.png'
-import CV from '../assets/files/CV for Prashant Man Gayak.pdf'
+import CV from '../assets/files/CV of Prashant Man Gayak.pdf'
 
 const Hero = () => {
   return (
@@ -35,7 +35,7 @@ const Hero = () => {
               <img src={XIcon} alt="Twitter" className='rounded-md'/>
             </a>
           </div>
-          <h1 class="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start"><span class="relative ">Prashant Man Gayak,</span><span class="whitespace-nowrap relative "><span class=" relative whitespace-nowrap"><span class="absolute bg-neutral-content -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1"></span><span class="relative text-neutral text-opacity-0 bg-transparent">Front End</span></span><span class="mr-3 sm:mr-4 md:mr-5 ml-4">developer</span></span></h1>
+          <h1 class="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4 flex flex-col gap-3 items-center lg:items-start"><span class="relative ">Prashant Man Gayak,</span><span class="whitespace-nowrap relative "><span class=" relative whitespace-nowrap"><span class="absolute bg-neutral-content -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1"></span><span class="relative text-neutral text-opacity-0 bg-transparent">Web</span></span><span class="mr-3 sm:mr-4 md:mr-5 ml-4">developer</span></span></h1>
         </div>
         <p className='text-lg lg:text-xl text-neutral-content my-5 md:my-0'>I bring websites to life as a front-end developer. I translate designs into user-friendly interfaces. My focus is on creating intuitive web experiences.</p>
         <div class="space-y-4">
